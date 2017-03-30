d7ba6bdcdac3c8d15a911e5a44068cba619fc159
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-03-30 18:40:12 +0200

    add checkEmail

--- script.js
+++ script.js
@@ -19,22 +19,64 @@
 
 
 
-(function checkCheckbox() {
+//(function checkCheckbox() {
+//    const errorDiv = document.getElementById("error");
+//    const orderForm = document.getElementById("order");
+//    const regulations = document.getElementById("regulations");
+//    
+//    
+//    
+//    
+//    
+//	orderForm.onsubmit = function() {
+//		if(regulations.checked == false) {
+//			errorDiv.innerHTML = "Accept regulations";
+//			return false;
+//		} else {
+//            errorDiv.innerHTML = "";
+//			return true;
+//		}
+//	}    
+//})();
+
+
+//(function checkQantity() {
+//    const errorDiv = document.getElementById("error");
+//    const orderForm = document.getElementById("order");
+//    const quantity = document.getElementById("quantity");
+//    
+//    
+//    
+//    
+//    
+//	orderForm.onsubmit = function() {
+//		if(isNaN(quantity.value) || quantity.value === "") {
+//			errorDiv.innerHTML = "Put number value in quantity";
+//			return false;
+//		} else {
+//            errorDiv.innerHTML = "";
+//			return true;
+//		}
+//	}    
+//})();
+
+
+(function checkEmail() {
     const errorDiv = document.getElementById("error");
     const orderForm = document.getElementById("order");
-    const regulations = document.getElementById("regulations");
+    const email = document.getElementById("email");
     
     
-    
+    const patternRegExp = /[@]/;
     
     
 	orderForm.onsubmit = function() {
-		if(regulations.checked == false) {
-			errorDiv.innerHTML = "Accept regulations";
-			return false;
-		} else {
+		if(email.value.match(patternRegExp)) {
             errorDiv.innerHTML = "";
 			return true;
+		} else {
+			errorDiv.innerHTML = "Email should look like this: info@server.com";
+			return false;
 		}
 	}    
 })();
